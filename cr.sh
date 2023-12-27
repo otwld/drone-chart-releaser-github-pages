1e1e7a1f58681a33901423086aad1d54af960c8d
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2023-12-27 16:07:02 +0100

    feat: working ci

--- cr.sh
+++ cr.sh
@@ -155,14 +155,7 @@ read_env_vars() {
 }
 
 install_chart_releaser() {
-  #if [[ ! -d "$RUNNER_TOOL_CACHE" ]]; then
-  #  echo "Cache directory '$RUNNER_TOOL_CACHE' does not exist" >&2
-  #  exit 1
-  #fi
-
-  if [[ ! -d "$install_dir" ]]; then
-    mkdir -p "$install_dir"
-
+  if [[ -d "$install_dir" ]]; then
     echo "Installing chart-releaser on $install_dir..."
     curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
     tar -xzf cr.tar.gz -C "$install_dir"
